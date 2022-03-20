28823268b86d60c8c3e8c1b6baa5f85403ae53e7
Garrett Austin Hurst <gahurst@ecegrid-thin3.ecn.purdue.edu>
2022-03-19 21:55:40 -0400

    dynamic for square odd sized mazes

--- src/main.c
+++ src/main.c
@@ -4,27 +4,30 @@
 #include "../inc/clog.h"
 
 int main(){
-    int rows, cols;
-	printf("Enter rows: ");
-	scanf("%d", &rows);
-	printf("Enter columns: ");
-	scanf("%d", &cols);
+    int size;
+	do {
+		printf("Enter size: ");
+		scanf("%d", &size);
+		if(size < 3 || size % 2 == 0) {
+			printf("Error: size must be greater than 3 and odd\n");
+		}
+	} while(size < 3 || size % 2 == 0);
 
-    char** mzStack = malloc(rows * sizeof(*mzStack));
-    for (int i = 0; i < cols; i++) {
-        mzStack[i] = malloc((cols + 1) * sizeof(*mzStack[i]));
-        mzStack[i][cols] = '\0';
+    char** mzStack = malloc(size * sizeof(*mzStack));
+    for (int i = 0; i < size; i++) {
+        mzStack[i] = malloc((size + 1) * sizeof(*mzStack[i]));
+        mzStack[i][size] = '\0';
     }
 
-    generateMaze(mzStack, rows, cols);       //generates the maze
+    generateMaze(mzStack, size, size);       //generates the maze
 
-    outputText(mzStack, rows, cols);        //outputs the maze to a text file
+    outputText(mzStack, size, size);        //outputs the maze to a text file
 
-    outputTerminal(mzStack, rows, cols);    //outputs the maze to the terminal
+    outputTerminal(mzStack, size, size);    //outputs the maze to the terminal
 
 
     // frees allocated memory
-    for (int i = 0; i < cols; i++) {
+    for (int i = 0; i < size; i++) {
         free(mzStack[i]);
     }
     free(mzStack);
